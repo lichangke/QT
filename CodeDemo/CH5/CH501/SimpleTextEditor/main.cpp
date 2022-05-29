d4efb7e8f82c1cb13371d182a0e53b20411f3bd7
lichangke <986740304@qq.com>
2022-05-29 16:35:12 +0800

    Update main.cpp

--- CodeDemo/CH5/CH501/SimpleTextEditor/main.cpp
+++ CodeDemo/CH5/CH501/SimpleTextEditor/main.cpp
@@ -5,6 +5,8 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    QFont f("楷体",12);            //设置显示的字体格式
+    a.setFont(f);
     SimpleTextEditor w;
     w.show();
     return a.exec();
